20bca766932fe832ae27d7065a233b3aaa1611b4
dsouzae <dsouzae@vmware.com>
2018-05-13 21:26:18 +0530

    Final working code for prediction

--- routes/visualization.js
+++ routes/visualization.js
@@ -13,62 +13,52 @@ router.get('/heat_map', function(req, res, next) {
 });
 
 router.get('/bar_index', function(req, res, next) {
-	var client = new cassandra.Client({contactPoints: ['127.0.0.1:9042'], keyspace: 'project'});
-	client.execute("SELECT * FROM bjp WHERE topicId='bjp_karnataka' ORDER BY time ASC ", function (err, result) {
-		if (!err){
-			console.log(result.rows.length + "");
-			if ( result.rows.length > 0 ) {
-				data = [];
-				data1 = [];
-				threshold = new Date();
-				threshold.setFullYear(2018, 3, 21);
-				var oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
+	const query = "SELECT * FROM congress_prediction WHERE topicname='congress_karnataka' ORDER BY time ASC ";
+	var client = new cassandra.Client({contactPoints: ['127.0.0.1:9042'], keyspace: 'prediction'});
+	data = [];
+	data1 = [];
+	threshold = new Date();
+	threshold.setFullYear(2018, 4, 12);
+	//var oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
+	var min = 99999999;
+	var max = -999999999;
+	var minTime = threshold
 
-				var min = 99999999;
-				var max = -999999999;
-
-				for(var i=0; i<result.rows.length; i++){
-					var tuple = {};
-					var tuple1 = {};
-					var time = new Date(result.rows[i].time);
-					// if(time.getTime() < threshold.getTime()){
-						tuple["x"] = time;
-						tuple["y"] = result.rows[i].topicpotential + "";
-						data.push(tuple);
-
-						if(result.rows[i].topicpotential < min){
-							min = result.rows[i].topicpotential;
-						}
-						if(result.rows[i].topicpotential > max){
-							max = result.rows[i].topicpotential;
-						}
-					// }
-				}
-
-				var a = max;
-				var b = (min - max)/ Math.pow(result.rows.length, 0.1);
-
-				for(var i=0; i<result.rows.length; i++){
-					var tuple1 = {};
-					var time = new Date(result.rows[i].time);
-
-					if(time.getTime() < threshold.getTime()){
-						var diffDays = Math.round(Math.abs((threshold.getTime() - time.getTime())/(oneDay)));
-						tuple1["x"] = time;
-						tuple1["y"] = a + b * Math.pow(diffDays, 0.1) + "";
-						data1.push(tuple1);
-					}
-				}
-
-				res.render('barindex', { title: 'Bar Index', data: data, data1: data1});
-			} else {
-				res.render('index2', { title: 'Home', message: 'No results'});
+	client.eachRow(query, [], { autoPage: true }, function (err, row) {
+		
+			var tuple = {};
+			var time = new Date(row.time);
+			tuple["x"] = time;
+			tuple["y"] = row.topicpotential;
+			data.push(tuple);
+			
+			if(row.topicpotential < min){
+				min = row.topicpotential;
 			}
+
+			if(time < minTime){
+				minTime = time;
+			}
+
+			if(row.topicpotential > max){
+				max = row.topicpotential;
+			}
+	
+	}, function(){
+		var a = max;
+		var b = (min - max)/ Math.pow(threshold.getTime() - minTime.getTime(), 0.1);
+
+		for(var i=0; i<data.length; i++){
+			var tuple1 = {};
+			var time = new Date(data[i]["x"]);
+			
+			var diffDays = Math.abs((threshold.getTime() - time.getTime()));
+			tuple1["x"] = time;
+			tuple1["y"] = a + (b * Math.pow(diffDays, 0.1));
+			data1.push(tuple1);
 		}
-		else{
-			res.render('error', { title: 'Error', message: err});
-			console.log(err);
-		}
+		console.log(data1)
+		res.render('barindex', { title: 'Bar Index', data: data, data1: data1});
 	});
 });
 
